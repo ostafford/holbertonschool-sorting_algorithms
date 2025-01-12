3e4031bc3ad061dd5abfe321cfa075449a9f2cfa
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2025-01-12 17:02:17 +1100

    created 3-files.

--- sort.h
+++ sort.h
@@ -42,7 +42,10 @@ void bubble_sort(int *array, size_t size);
 void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
-
+void swap_ints(int *a, int *b);
+int lomuto_partition(int *array, size_t size, int left, int right);
+void lomuto_sort(int *array, size_t size, int left, int right);
+void quick_sort(int *array, size_t size);
 
 
 #endif /* SORT_H */
